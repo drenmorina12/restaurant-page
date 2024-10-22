f2a44edd60d1f1b94fd4d397d59d8014fe7156e1
Dren Morina <drenmorina321@gmail.com>
2024-10-22 19:56:58 +0200

    Create home.js and add code to populate home page

---
+++ src/home.js
@@ -0,0 +1,48 @@
+import { electron } from "webpack";
+
+const contentDiv = document.querySelector("#content");
+
+function createHome() {
+  const div = document.createElement("div");
+  div.setAttribute("id", "title");
+
+  const text = document.createElement("p");
+  text.textContent = "Beary's Breakfast Bar";
+
+  div.appendChild(text);
+  contentDiv.appendChild(div);
+}
+
+// function createFirstBox() {
+//   const div = document.createElement("div");
+//   div.setAttribute("class", "info-box");
+
+//   const text = document.createElement("p");
+//   text.setAttribute("class", "text");
+//   text.textContent = `Beary's has the best porridge! The atmosphere and customer service
+//   make you feel like you are sitting in the middle of the woods, eating
+//   like a bear! This is exactly the kind of place that I like to return
+//   to again and again.`;
+
+//   const author = document.createElement("p");
+//   author.setAttribute("class", "author");
+//   author.textContent = "Goldilocks";
+
+//   div.appendChild(text);
+//   div.appendChild(author);
+
+//   contentDiv.appendChild(div);
+// }
+
+// function createSecondBox() {}
+
+// function createThirdBox() {}
+
+// function createHome() {
+//   createTitle();
+//   createFirstBox();
+//   createSecondBox();
+//   createThirdBox();
+// }
+
+export { createHome };
